ba87c82243c021e1efdbac13c79d3de77427ed0c
Danny Sanchez <dannysanchez559@gmail.com>
2021-07-13 15:49:01 -0700

    chore(poke-app): finish initial setup, add file structure, install eslint, prettier setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,13 +8,13 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <img src="pokeball.png" alt="" />
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
