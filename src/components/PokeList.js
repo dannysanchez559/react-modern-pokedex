4ec75aff0ce60fc4dedcad48c192bbd90e746f94
connielion <clai011@ucr.edu>
2021-07-16 09:35:33 -0700

    commit before pulling gj-15-filter-menu

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,7 +1,12 @@
 import React from "react";
 import PokeCard from "./PokeCard";
 
-const PokeList = ({ userDidSearch, setUserDidSearch, fetchedData, allPokemons }) => {
+const PokeList = ({
+  userDidSearch,
+  setUserDidSearch,
+  fetchedData,
+  allPokemons,
+}) => {
   const { id, name, sprites, types } = fetchedData;
   let typeTags, sprite;
 
@@ -12,19 +17,28 @@ const PokeList = ({ userDidSearch, setUserDidSearch, fetchedData, allPokemons })
     });
   }
 
-
-   const allCards = allPokemons.map(pokeObj=>{
-     const {id, name, sprites, types} = pokeObj;
-     let typeTags = types.map((obj, i) => {
+  const allCards = allPokemons.map((pokeObj) => {
+    const { id, name, sprites, types } = pokeObj;
+    let typeTags = types.map((obj, i) => {
       return <span key={i}>{obj["type"]["name"]} </span>;
-     });
-     let sprite = sprites["other"]["official-artwork"]["front_default"];
-     return <PokeCard dexNo={id} name={name} sprite={sprite} typeTags={typeTags} key={id}/>
- })
+    });
+    let sprite = sprites["other"]["official-artwork"]["front_default"];
+    return (
+      <PokeCard
+        dexNo={id}
+        name={name}
+        sprite={sprite}
+        typeTags={typeTags}
+        key={id}
+      />
+    );
+  });
 
   return (
     <div>
-    {!userDidSearch?  <>{allCards}</>: fetchedData.length < 1 ? (
+      {!userDidSearch ? (
+        <>{allCards}</>
+      ) : fetchedData.length < 1 ? (
         <h1>Please search pokemon.</h1>
       ) : (
         <PokeCard dexNo={id} name={name} sprite={sprite} typeTags={typeTags} />
