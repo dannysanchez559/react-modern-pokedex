78939c64db61bcbf7ff8f77d3b995ab6164a39c1
Danny Sanchez <dannysanchez559@gmail.com>
2021-07-20 16:41:59 -0700

    feat(modal): implement pokecry, add pokecry speaker img, and speakOff img

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import PokemonDetails from "./PokemonDetails";
 import closeButton from "../img/closeButton.png";
 import crySpeaker from "../img/speaker.png";
+import speakerOff from "../img/speakerOff.png";
 
 export const Modal = (props) => {
+  const [cryUrlIsValid, setCryUrlIsValid] = useState(true);
+  const speakerRef = useRef();
+  const speakerButtonRef = useRef();
+
+  // handle cry error
+  const handleCryUrlError = () => {
+    if (speakerRef.current) speakerRef.current.src = speakerOff;
+    if (speakerButtonRef.current) speakerButtonRef.current.disabled = true;
+
+    console.log(speakerRef);
+  };
+
+  // url for pokemon cry
+  const cryUrl = `https://play.pokemonshowdown.com/audio/cries/${props.name}.mp3`;
+  const cry = new Audio(cryUrl);
+  cry.onerror = handleCryUrlError;
+
+  const playPokemonCry = () => {
+    cry.play();
+  };
+
   return (
     <>
       <div className="backgroundModalWrapper">
@@ -26,8 +48,8 @@ export const Modal = (props) => {
         <div className="modalDetailsBottom">
           <div className="nameContainer">
             <h1 className="modalPokeName">{props.name}</h1>
-            <button>
-              <img src={crySpeaker} />
+            <button onClick={playPokemonCry} ref={speakerButtonRef}>
+              <img src={crySpeaker} ref={speakerRef} />
             </button>
           </div>
 
