6c6df8dc84e5b7e1908d7fd1801d05722a32d729
connielion <clai011@ucr.edu>
2021-07-29 11:27:33 -0700

    refactor(poke-app): uncommented getMovesByPokemon, added type data for moves in Moves

--- src/components/PokeApp.js
+++ src/components/PokeApp.js
@@ -5,9 +5,11 @@ import {
   fetchPokemon,
   fetchAllPokemons,
   fetchSpecies,
+  fetchMove
 } from "../util/fetchPokemonData";
 import "../styles/pokeAppStyle.css";
 import "../styles/main.scss";
+
 
 function PokeApp() {
   const [pokemonName, setPokemonName] = useState("");
@@ -66,8 +68,39 @@ function PokeApp() {
     }
   };
 
+  // get data for n number of moves for each Pokemon
+  const getPokemonMoveset = async (moves, n) => {
+    const moveSet = [];
+    // loop over a pokemon's moves array N=4 times
+    for(let i = 0; i < n; i+=1){
+      const moveObj = moves[i];
+      const moveUrl = moveObj.move.url;
+      try {
+        const moveData = await fetchMove(moveUrl);
+        if(moveData){
+          moveSet.push(moveData);
+        }
+      } catch(error){
+        console.error(error);
+      }
+
+    }
+    // return a moveSet array
+    return moveSet;
+  }
+
+  const getMovesByPokemon = async (dexNo) => {
+    const moves = allPokemons.map((poke,i)=>{
+      const moves = poke.moves;
+      // get 4 moves from all moves a pokemon can learn
+      const fourMoves = getPokemonMoveset(moves, 4);
+      return fourMoves;
+    })
+  }
+
   useEffect(() => {
     // get all data: pagination/scroll, pokemon urls array
+    getMovesByPokemon(1);
     getAllPokemon();
   }, []);
 
