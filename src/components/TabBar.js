57bd43740c6a2e38b7bea5fdf8b7e6d5ea807783
connielion <clai011@ucr.edu>
2021-08-02 14:14:54 -0700

    refactor(PokeApp): move service worker to Public folder, remove Typography import in TabBar

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import BaseStats from "./BaseStats";
 import EvolutionTab from "./EvolutionTab";
