a9a781f02d815683c467c0237e93b6e379c03033
Garrett Johnson <garrettjtrains@gmail.com>
2021-08-06 14:48:10 -0700

    feat(modal)refactored function to change null values to 0, style updates to graph content

--- src/components/Moves.js
+++ src/components/Moves.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
-// import bug from "../img/typeIcons/bug.png";
-// import normal from "../img/typeIcons/normal.png";
-// import flying as Flying from '../img/typeIcons/images';
-// import solution from stackoverflow
+
 import {
   Bug,
   Dark,
@@ -55,41 +52,43 @@ const Moves = ({ moveSet }) => {
       }
     }
   };
-  console.log(`normal type check`, typeof Normal);
+
+  const movePower = (accuracy, power, pp) => {
+    accuracy = accuracy ? accuracy : 0;
+    power = power ? power : 0;
+    pp = pp ? pp : 0;
+    return [accuracy, power, pp];
+  };
 
   const moveInfo = moveSet.map((moveObj) => {
     let { accuracy, name, power, pp, flavor_text_entries, type } = moveObj;
     // call function to loop through flavor_text_entries and find "en"
     const typeName = type.name;
+    const result = movePower(accuracy, power, pp);
 
-    // if movetype has a null value, return 0
-    {
-      accuracy = accuracy ? accuracy : 0;
-    }
-    {
-      power = power ? power : 0;
-    }
-    {
-      pp = pp ? pp : 0;
-    }
+    //get movetype icon
     const typeIcon = imageUrls[`${typeName}`];
 
-    console.log(`typeicon url`, typeIcon);
     return (
       <>
         <div className="movesGraphContainer">
-          <div>
-            <p>{`${name.toUpperCase()}`}</p>{" "}
-            <p>{getEngFlavorText(flavor_text_entries)}</p>
-            <img src={`${typeIcon}`} alt="type icon" />
-          </div>
+          <div className="graphTitleContent">
+            <div className="iconTitleContainer">
+              <img src={`${typeIcon}`} alt="type icon" />
+              <p> {`${name.toUpperCase()}`} </p>{" "}
+            </div>
+            <p className="moveDescription">
+              {" "}
+              {getEngFlavorText(flavor_text_entries)}{" "}
+            </p>{" "}
+          </div>{" "}
           <Bar
             className="movesGraph"
             data={{
               labels: ["Power", "Accuracy", "PP"],
               datasets: [
                 {
-                  data: [power, accuracy, pp],
+                  data: [...result],
                   backgroundColor: ["#CE2121", "#419516", "#4986CF"],
                   hoverBackgroundColor: ["#CE2121", "#419516", "#4986CF"],
                   borderRadius: 50,
@@ -138,27 +137,6 @@ const Moves = ({ moveSet }) => {
                 legend: {
                   display: false,
                 },
-                // title: {
-                //   display: true,
-                //   text: name.toUpperCase(),
-                //   // getEngFlavorText(flavor_text_entries),
-                //   color: "#111",
-                //   align: "start",
-                //   font: {
-                //     size: 18,
-                //   },
-                // },
-                // subtitle: {
-                //   display: true,
-                //   text: getEngFlavorText(flavor_text_entries),
-                //   align: "start",
-                //   padding: {
-                //     bottom: 15,
-                //   },
-                //   font: {
-                //     size: 14,
-                //   },
-                // },
                 tooltip: {
                   enabled: true,
                   backgroundColor: "rgb(0,0,0,0.6",
@@ -168,20 +146,12 @@ const Moves = ({ moveSet }) => {
                 },
               },
             }}
-          />
-          {/* <ul key={name}>
-        <li>{name.toUpperCase()}</li>
-        <li>{type.name}</li>
-        <li>{getEngFlavorText(flavor_text_entries)}</li>
-        <li>Accuracy: {accuracy}</li>
-        <li>Power: {power}</li>
-        <li>PP: {pp}</li>
-      </ul> */}
-        </div>
+          />{" "}
+        </div>{" "}
       </>
     );
   });
-  return <div className="moves-panel">{moveInfo}</div>;
+  return <div className="moves-panel"> {moveInfo} </div>;
 };
 
 export default Moves;
