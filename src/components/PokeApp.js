75667050442919d5fe5ef9ba92e55813ae1f5a22
connielion <clai011@ucr.edu>
2021-08-16 10:03:40 -0700

    fix(PokeApp): Add trim() to the end of searchbar value to trim whitespaces

--- src/components/PokeApp.js
+++ src/components/PokeApp.js
@@ -18,7 +18,7 @@ const PokeApp = () => {
   const [isLoading, setIsLoading] = useState(true);
   // search bar onChange function for searching pokemon & conditional rendering
   const handleNameSearch = (e) => {
-    const searchBarValue = e.target.value;
+    const searchBarValue = e.target.value.trim();
     setPokemonName(searchBarValue);
   };
 
